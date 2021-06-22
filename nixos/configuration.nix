7547dda5db53b43e25fadd972ce945590f9cc636
heyduda <heyduda@pm.me>
2021-06-22 16:43:38 +0200

    cleaning up configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,5 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
@@ -11,10 +10,10 @@
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "milesobrian"; # Define your hostname.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
@@ -22,13 +21,19 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp2s0.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking = {
+    hostName = "milesobrian";
+    useDHCP = false;
+    interfaces = {
+      enp2s0.useDHCP = true;
+      wlp3s0.useDHCP = true;
+    };
+    firewall = {
+      enable = false;
+      allowedTCPPorts = [ ... ];
+      allowedUDPPorts = [ ... ];
+    };
+  };
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -38,76 +43,54 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable Pantheon Desktop
-  services.xserver.desktopManager.pantheon.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "intl";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager.pantheon.enable = true;
+      layout = "us";
+      xkbVariant = "intl";
+      libinput.enable = true;
+    };
+    printing.enable = true;
+    openssh.enable = true;
+    k3s = {
+      enable = true;
+      extraFlags = "--write-kubeconfig-mode 644";
+    };
+  };
 
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-
-  environment.variables = {
-    DEFAULT_USER = "earthling";
-    KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   nixpkgs.config.allowUnfree = true;
-  environment.systemPackages = with pkgs; [
-    neovim
-    firefox
-    kubectl
-    kubernetes-helm
-    git
-    gh
-    tig
-    tmux
-    vscode
-    vscode-extensions.redhat.vscode-yaml
-    vscode-extensions.mskelton.one-dark-theme
-    vscode-extensions.ms-kubernetes-tools.vscode-kubernetes-tools
-    vscode-extensions.editorconfig.editorconfig
-  ];
+  environment = {
+    variables = {
+      DEFAULT_USER = "earthling";
+      KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+    };
+    systemPackages = with pkgs; [
+      neovim
+      firefox
+      kubectl
+      kubernetes-helm
+      git
+      gh
+      tig
+      tmux
+      vscode
+      vscode-extensions.redhat.vscode-yaml
+      vscode-extensions.mskelton.one-dark-theme
+      vscode-extensions.ms-kubernetes-tools.vscode-kubernetes-tools
+      vscode-extensions.editorconfig.editorconfig
+    ];
+  };
 
   fonts.fonts = with pkgs; [
     fira-code
     fira-code-symbols
     powerline-fonts
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-  services.openssh.enable = true;
-  services.k3s = {
-    enable = true;
-    extraFlags = "--write-kubeconfig-mode 644";
-  };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
 
   programs.zsh = {
     enable = true;
@@ -118,7 +101,6 @@
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.earthling = {
     isNormalUser = true;
     shell = pkgs.zsh;
@@ -130,12 +112,5 @@
     dockerCompat = true;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "21.05";
 }
-
