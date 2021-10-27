fb480776937f7a04bfb51646bb74231234ffc9ef
Thorsten Aye <80338592+thorsten-eficode@users.noreply.github.com>
2021-10-27 17:41:08 +0200

    feature/elementaryos with ansible (#4)

---
+++ elementaryos/roles/base/files/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+  set -g theme_display_k8s_context yes
+  set -g theme_display_virtualenv yes
+  set -g theme_display_user ssh
+  set -g theme_display_hostname ssh
+  set -g theme_display_cmd_duration yes
+  set -g theme_powerline_fonts yes
+  set -g theme_nerd_fonts no
+  set -g theme_show_exit_status yes
+  set -g theme_display_jobs_verbose yes
+  set -g theme_color_scheme nord
+end
---
+++ elementaryos/roles/base/files/neovim/init.vim
@@ -0,0 +1,8 @@
+set laststatus=2
+set t_Co=256
+set number
+set cursorline
+set nocompatible
+syntax on
+colorscheme nord
+filetype indent plugin on
